5f7c405cbf870b2eab0a94d92177369aecb36f64
Harry James <harryjamesuk@users.noreply.github.com>
2016-10-08 22:51:14 +0100

    Add node.js web server support
    This app can also be used as a webapp :)

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 // Create web server
 const http = require('http');
 var server = http.Server(app);
 server.listen(process.env.PORT || 3000);
 
+// Public dirs
+app.use(express.static('public'));
+app.use('/js', express.static('www/js'));
+app.use('/css', express.static('www/css'));
+app.use('/lib/ionic', express.static('www/lib/ionic'))
+
 app.get('/', function(req, res) {
 
   res.writeHead(200, {'Content-Type': 'text/html'});
 
-  res.write('www/index.html');
+  var view = fs.readFileSync("./www/index.html", {encoding: "utf-8"});
+  res.write(view);
+
   res.end();
 });
